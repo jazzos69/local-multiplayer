8e816179b992aaa0e54fa13a611199f910030b6a
jazzos69 <410005651@st.roc.a12.nl>
2025-01-22 12:59:15 +0100

    vernieuwing

---
+++ 2D-local-multiplayer/Assets/playerelement.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public enum ElementType
+{
+    Fire,
+    Water
+}
+
+public class PlayerElement : MonoBehaviour
+{
+    public ElementType element; // zet het element in de inspector
+}
+
